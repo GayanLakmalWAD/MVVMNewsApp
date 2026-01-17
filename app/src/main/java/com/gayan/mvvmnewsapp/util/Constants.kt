30f57b4d9338370422f22216512b2e5741377fb8
Android Lakmal <gayanlakmalwad@gmail.com>
2026-01-17 11:48:13 +0530

    setup-retrofit

---
+++ app/src/main/java/com/gayan/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.gayan.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "aa3c5ed3d6fd4ef2946b4cecd32ddfcc"
+        const val BASE_URL = "https://newsapi.org/"
+
+    }
+}
