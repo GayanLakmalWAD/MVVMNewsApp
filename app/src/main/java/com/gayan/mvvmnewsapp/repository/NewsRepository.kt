b3d9d7b6a95ef0aca22bdd05c47503e74d3a7459
Android Lakmal <gayanlakmalwad@gmail.com>
2026-01-17 20:02:17 +0530

    articles save and delete

--- app/src/main/java/com/gayan/mvvmnewsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/gayan/mvvmnewsapp/repository/NewsRepository.kt
@@ -2,6 +2,7 @@ package com.gayan.mvvmnewsapp.repository
 
 import com.gayan.mvvmnewsapp.api.RetrofitInstance
 import com.gayan.mvvmnewsapp.db.ArticleDatabase
+import com.gayan.mvvmnewsapp.models.Article
 
 /** The Repository acts as the "Single Source of Truth." Its job is to decide whether to
  * fetch data from the network (API) or from the local database (ArticleDatabase)
@@ -13,6 +14,12 @@ class NewsRepository(
     suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
         RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
 
-    suspend fun  searchNews(searchQuery: String, pageNumber: Int) =
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
         RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
+
+    fun getSavedNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticles(article: Article) = db.getArticleDao().deleteArticle(article)
 }
