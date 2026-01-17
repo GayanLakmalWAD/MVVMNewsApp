5bf0645426075bdb2c3296c3304b665b65c474a8
Android Lakmal <gayanlakmalwad@gmail.com>
2026-01-17 20:02:17 +0530

    articles save and delete

--- app/src/main/java/com/gayan/mvvmnewsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/gayan/mvvmnewsapp/ui/fragments/ArticleFragment.kt
@@ -4,12 +4,27 @@ import android.os.Bundle
 import android.view.View
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
 import com.gayan.mvvmnewsapp.R
 import com.gayan.mvvmnewsapp.databinding.FragmentArticleBinding
+import com.gayan.mvvmnewsapp.db.ArticleDatabase
+import com.gayan.mvvmnewsapp.db.ArticleDatabase.Companion.invoke
+import com.gayan.mvvmnewsapp.repository.NewsRepository
+import com.gayan.mvvmnewsapp.ui.NewsViewModel
+import com.gayan.mvvmnewsapp.ui.NewsViewModelProviderFactory
+import com.google.android.material.snackbar.Snackbar
+import kotlin.getValue
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
 
+    private val viewModel: NewsViewModel by activityViewModels {
+        NewsViewModelProviderFactory(
+            NewsRepository(
+                ArticleDatabase(requireContext())
+            )
+        )
+    }
     private var _binding: FragmentArticleBinding? = null
     private val binding get() = _binding!!
     val args: ArticleFragmentArgs by navArgs()
@@ -23,6 +38,11 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
         binding.webView.apply{
             webViewClient = WebViewClient()
             loadUrl(article.url)
+        }
+        binding.fab.setOnClickListener {
+            viewModel.saveArticle(article)
+            Snackbar.make(view,
+                "Article saved successfully", Snackbar.LENGTH_SHORT).show()
         }
     }
 
--- app/src/main/java/com/gayan/mvvmnewsapp/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/gayan/mvvmnewsapp/ui/fragments/SavedNewsFragment.kt
@@ -3,14 +3,31 @@ package com.gayan.mvvmnewsapp.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.gayan.mvvmnewsapp.R
 import com.gayan.mvvmnewsapp.adapters.NewsAdapter
 import com.gayan.mvvmnewsapp.databinding.FragmentSavedNewsBinding
+import com.gayan.mvvmnewsapp.db.ArticleDatabase
+import com.gayan.mvvmnewsapp.repository.NewsRepository
+import com.gayan.mvvmnewsapp.ui.NewsViewModel
+import com.gayan.mvvmnewsapp.ui.NewsViewModelProviderFactory
+import com.google.android.material.snackbar.Snackbar
+import kotlin.getValue
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
 
+    private val viewModel: NewsViewModel by activityViewModels {
+        NewsViewModelProviderFactory(
+            NewsRepository(
+                ArticleDatabase(requireContext())
+            )
+        )
+    }
     private lateinit var newsAdapter: NewsAdapter
 
     private var _binding: FragmentSavedNewsBinding? = null
@@ -32,6 +49,43 @@ class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
                 bundle
             )
         }
+
+        val itemTouchHelper = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT,
+            ){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return  true
+            }
+
+            override fun onSwiped(
+                viewHolder: RecyclerView.ViewHolder,
+                direction: Int
+            ) {
+                val position = viewHolder.getAbsoluteAdapterPosition()
+                val article = newsAdapter.differ.currentList[position]
+                viewModel.deleteArticle(article)
+                Snackbar.make(view, "Successfully deleted article", Snackbar.LENGTH_LONG).apply {
+                    setAction("Undo"){
+                        viewModel.saveArticle(article)
+                    }
+                    show()
+                }
+            }
+        }
+
+        ItemTouchHelper(itemTouchHelper).apply {
+            attachToRecyclerView(binding.rvSavedNews)
+        }
+
+        viewModel.getSavedNews().observe(viewLifecycleOwner, Observer{articles ->
+            newsAdapter.differ.submitList(articles)
+        })
+
     }
 
     private fun setupRecyclerView() {
@@ -41,4 +95,5 @@ class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
             layoutManager = LinearLayoutManager(requireContext())
         }
     }
+
 }
